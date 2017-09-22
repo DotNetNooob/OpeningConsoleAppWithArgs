55f95b677ba249617b1064dd5c93a13c1ce8c44c
Muhammad Ansab Iqbal <ansab.iqbal@ba.com>
2017-09-22 10:04:37 +0500

    Initial

---
+++ TriggerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriggerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path =
+                @"C:\Users\ansab.iqbal\Documents\Visual Studio 2015\Projects\Console Projects\OpeningConsoleAppWithArgs\OpeningConsoleAppWithArgs\bin\Debug\OpeningConsoleAppWithArgs.exe";
+            string Arguments = "LikedBusiness SignUp Register";
+
+            System.Diagnostics.Process.Start(Path, Arguments);
+
+            Console.WriteLine("Process Started");
+            Console.ReadKey();
+
+        }
+    }
+}
